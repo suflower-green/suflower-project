d4da11feb50a01f9bb39be537d72c9d30b6a68a1
jaek1997 <jaek1997@gmail.com>
2022-01-31 22:23:33 +0900

    updated OrderDTO

--- suflower/src/main/java/com/suflower/domain/OrderDTO.java
+++ suflower/src/main/java/com/suflower/domain/OrderDTO.java
@@ -6,9 +6,11 @@ import lombok.Data;
 public class OrderDTO {
 	
 	private Long orderId;
+	private Long productId;
 	private int orderQuantity;
 	private String orderDate;
 	private String reservationDate;
+	private String memberId;
 	private String receiverName;
 	private String receiverAddr1;
 	private String receiverAddr2;
