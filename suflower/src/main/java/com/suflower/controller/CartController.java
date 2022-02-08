a262692320ecebc9831252171d3f3d57fa35fe9d
jaek1997 <jaek1997@gmail.com>
2022-02-09 03:00:14 +0900

    updated CartController

--- suflower/src/main/java/com/suflower/controller/CartController.java
+++ suflower/src/main/java/com/suflower/controller/CartController.java
@@ -4,28 +4,42 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.suflower.domain.CartDTO;
 import com.suflower.service.CartService;
 
 @Controller
 @RequestMapping("/cart/*")
+@ResponseBody
 public class CartController {
-	
+
 	private final CartService service;
 
 	public CartController(CartService service) {
-		
+
 		this.service = service;
 	}
 
+	@RequestMapping("/insert")
+	public void cartInsert(CartDTO cart, HttpServletRequest request) throws Exception {
+
+		HttpSession session = request.getSession();
+		String loginId = (String) session.getAttribute("loginId");
+		if (loginId != null) {
+			cart.setMemberId(loginId);
+			service.insert(cart);
+		}
+
+	}
 	
-	
-	
-	
+	@RequestMapping("/cartList")
+	public ModelAndView cartList(ModelAndView mv) {
+		
+		mv.setViewName("/cart/cartList");
+		return mv;
+	}
+
 }
